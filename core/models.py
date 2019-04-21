1e485f7a65e8582e600a25fc9dc3dc8592231bef
MakuZo <MakuZo90@gmail.com>
2019-04-21 19:29:51 +0200

    Restructured database/models. Added all migrations to repository.

--- core/models.py
+++ core/models.py
@@ -5,27 +5,23 @@ class Food(models.Model):
     """
     Model for Food object (from 'FOOD_DES' table).
 
-    :desc_long    - 200-character description of food item
-    :desc_short   - 60-character abbreviated description of food item
+    :name         - 100-character name of product, should be concise 
+                    (no adjectives etc.). It's the main field that will be 
+                    used when matching ingredients from recipes to products from db.
+    :description  - 200-character description of food item 
+                    (e.g if it's raw or cooked, skin only, etc.)
     :common_name  - Other names commonly used to describe a food,
                     including local or regional names, such as “soda” or
                     “pop” for “carbonated beverages”
-    :manufac_name - The company that manufactured the product, when appropriate
-    :refuse_perc  - Percentage of refuse by weight (refuse is inedible parts of a food item)
-    :sci_name     - Scientific name of the food item for the least
-                    processed form of the food (usually raw), if applicable
     :n_factor     - Factor for converting nitrogen to protein amounts
     :pro_factor   - Factor for calculating calories from protein amounts
     :fat_factor   - Factor for calculating calories from fat levels
     :cho_factor   - Factor for calculating calories from carbohydrate values
     """
 
-    desc_long = models.CharField(max_length=200)
-    desc_short = models.CharField(max_length=60)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True, blank=True)
     common_name = models.CharField(max_length=100, null=True, blank=True)
-    manufac_name = models.CharField(max_length=65, null=True, blank=True)
-    refuse_perc = models.IntegerField(null=True, blank=True)
-    sci_name = models.CharField(max_length=65, null=True, blank=True)
     n_factor = models.DecimalField(
         max_digits=4, decimal_places=2, null=True, blank=True
     )
@@ -39,8 +35,8 @@ class Food(models.Model):
         max_digits=4, decimal_places=2, null=True, blank=True
     )
 
-    def __str__(self): # pragma: no cover
-        return f"#{self.id} {self.desc_long}"
+    def __str__(self):  # pragma: no cover
+        return f"#{self.id} {self.name}"
 
 
 class FoodWeight(models.Model):
@@ -48,25 +44,18 @@ class FoodWeight(models.Model):
     FoodWeight model contains the weights in grams of several common measures for each food item.
 
     :food        - Food object
-    :seq         - Sequence number
     :amount      - Unit modifier (for example, 1 in “1 cup”)
     :desc        - Description (for example, “cup, diced,” or “1-inch pieces”)
-    :weight      - Weight in grams
-    :data_points - Number of data points
+    :value       - Weight in grams
     :deviation   - Standard deviation
     """
 
     food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name="weight")
-    seq = models.IntegerField()
     amount = models.DecimalField(max_digits=6, decimal_places=3)
     desc = models.CharField(max_length=84)
-    weight = models.DecimalField(max_digits=7, decimal_places=1)
-    data_points = models.IntegerField(null=True, blank=True)
-    deviation = models.DecimalField(
-        max_digits=7, decimal_places=3, null=True, blank=True
-    )
+    value = models.DecimalField(max_digits=7, decimal_places=1)
 
-    def __str__(self): # pragma: no cover
+    def __str__(self):  # pragma: no cover
         return f"{self.desc} of {self.food}"
 
 
@@ -78,8 +67,6 @@ class FoodNutrition(models.Model):
     :desc    - Name of nutrient/food component
     :value   - Amount in 100 g, edible portion
     :units   - Units of measure (e.g., mg, g, and μg)
-    :min_val - Minimum value
-    :max_val - Maximum value
     :tagname - International Network of Food Data Systems tagname
     """
 
@@ -87,13 +74,7 @@ class FoodNutrition(models.Model):
     desc = models.CharField(max_length=60)
     value = models.DecimalField(max_digits=10, decimal_places=3)
     units = models.CharField(max_length=7)
-    min_val = models.DecimalField(
-        max_digits=10, decimal_places=3, null=True, blank=True
-    )
-    max_val = models.DecimalField(
-        max_digits=10, decimal_places=3, null=True, blank=True
-    )
     tagname = models.CharField(max_length=20, null=True, blank=True)
 
-    def __str__(self): # pragma: no cover
+    def __str__(self):  # pragma: no cover
         return f"{self.tagname} of {self.food}"
--- core/srlegacy/sr_database.py
+++ core/srlegacy/sr_database.py
@@ -1,9 +1,10 @@
 """
 Script which imports data from USDA's National Nutrient Database for Standard Reference
 """
-import food
-import nutrition
-import weight
+from core.srlegacy import food
+from core.srlegacy import nutrition
+from core.srlegacy import weight
+from core.srlegacy import correct_weights
 
 PATH_TO_FOOD = "FOOD_DES.txt"
 PATH_TO_WEIGHT = "WEIGHT.txt"
@@ -11,6 +12,7 @@ PATH_TO_NUTRITION = "NUT_DATA.txt"
 PATH_TO_DEF_DATA = "NUTR_DEF.txt"
 
 
-food.main(PATH_TO_FOOD_DES)
+food.main(PATH_TO_FOOD)
 weight.main(PATH_TO_WEIGHT)
 nutrition.main(PATH_TO_NUTRITION, PATH_TO_DEF_DATA)
+correct_weights.correct()
