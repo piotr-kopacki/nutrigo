7495fa32f355f61206b773a98425356dc77f9360
MakuZo <MakuZo90@gmail.com>
2019-07-07 13:37:05 +0200

    Added docs for IngredientsSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,5 +2,6 @@ from rest_framework import serializers
 
 
 class IngredientsSerializer(serializers.Serializer):
+    """Serializer used to validate POST data, but not to return it"""
     ingredients = serializers.ListField(child=serializers.CharField(), allow_empty=False)
     servings = serializers.IntegerField(required=False, min_value=1)
